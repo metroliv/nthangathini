411153502faa442daafc74c271e242b9485458f1
metroliv <135633599+metroliv@users.noreply.github.com>
2024-01-06 15:38:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+function showsong1() {
+	hideAllSections();
+    document.getElementById("song1").style.display = "block";
+}
+function showsong2() {
+	hideAllSections();
+    document.getElementById("song2").style.display = "block";
+}
+function showsong3() {
+	hideAllSections();
+    document.getElementById("song3").style.display = "block";
+}
+function showsong4() {
+	hideAllSections();
+    document.getElementById("song4").style.display = "block";
+}
+function showsong5() {
+	hideAllSections();
+    document.getElementById("song5").style.display = "block";
+}
+function showsong6() {
+	hideAllSections();
+    document.getElementById("song6").style.display = "block";
+}
+function showsong7() {
+	hideAllSections();
+    document.getElementById("song7").style.display = "block";
+}
+
+function hideAllSections() {
+    document.getElementById("song1").style.display = "none";
+    document.getElementById("song2").style.display = "none";
+    document.getElementById("song3").style.display = "none";
+    document.getElementById("song4").style.display = "none";
+    document.getElementById("song5").style.display = "none";
+    document.getElementById("song6").style.display = "none";
+    document.getElementById("song7").style.display = "none";
+}
